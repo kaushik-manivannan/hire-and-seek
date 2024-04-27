f0a32ab42a9d91a38128fc6186725c2687952d82
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-04-26 20:54:31 -0400

    Fix Anchor Tag Links

--- src/main/java/com/kaushikmanivannan/hireandseek/HireandseekApplication.java
+++ src/main/java/com/kaushikmanivannan/hireandseek/HireandseekApplication.java
@@ -2,9 +2,16 @@ package com.kaushikmanivannan.hireandseek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HireandseekApplication {
+public class HireandseekApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HireandseekApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HireandseekApplication.class, args);
